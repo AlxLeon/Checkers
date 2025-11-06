e86994a868c4d4fb3eddd121bf3a7d1e0c117419
Alex <leon.express@gmail.com>
2025-11-06 09:29:41 +0300

    Local changes

--- Game/Hand.h
+++ Game/Hand.h
@@ -6,97 +6,77 @@
 #include "Board.h"
 
 // methods for hands
-class Hand
-{
-  public:
-    Hand(Board *board) : board(board)
-    {
-    }
-    tuple<Response, POS_T, POS_T> get_cell() const
-    {
-        SDL_Event windowEvent;
-        Response resp = Response::OK;
-        int x = -1, y = -1;
-        int xc = -1, yc = -1;
-        while (true)
-        {
-            if (SDL_PollEvent(&windowEvent))
-            {
-                switch (windowEvent.type)
-                {
-                case SDL_QUIT:
-                    resp = Response::QUIT;
-                    break;
-                case SDL_MOUSEBUTTONDOWN:
-                    x = windowEvent.motion.x;
-                    y = windowEvent.motion.y;
-                    xc = int(y / (board->H / 10) - 1);
-                    yc = int(x / (board->W / 10) - 1);
-                    if (xc == -1 && yc == -1 && board->history_mtx.size() > 1)
-                    {
-                        resp = Response::BACK;
-                    }
-                    else if (xc == -1 && yc == 8)
-                    {
-                        resp = Response::REPLAY;
-                    }
-                    else if (xc >= 0 && xc < 8 && yc >= 0 && yc < 8)
-                    {
-                        resp = Response::CELL;
-                    }
-                    else
-                    {
-                        xc = -1;
-                        yc = -1;
-                    }
-                    break;
-                case SDL_WINDOWEVENT:
-                    if (windowEvent.window.event == SDL_WINDOWEVENT_SIZE_CHANGED)
-                    {
-                        board->reset_window_size();
-                        break;
-                    }
-                }
-                if (resp != Response::OK)
-                    break;
-            }
+class Hand {
+public:
+  Hand(Board *board) : board(board) {}
+  tuple<Response, POS_T, POS_T> get_cell() const {
+    SDL_Event windowEvent;
+    Response resp = Response::OK;
+    int x = -1, y = -1;
+    int xc = -1, yc = -1;
+    while (true) {
+      if (SDL_PollEvent(&windowEvent)) {
+        switch (windowEvent.type) {
+        case SDL_QUIT:
+          resp = Response::QUIT;
+          break;
+        case SDL_MOUSEBUTTONDOWN:
+          x = windowEvent.motion.x;
+          y = windowEvent.motion.y;
+          xc = int(y / (board->H / 10) - 1);
+          yc = int(x / (board->W / 10) - 1);
+          if (xc == -1 && yc == -1 && board->history_mtx.size() > 1) {
+            resp = Response::BACK;
+          } else if (xc == -1 && yc == 8) {
+            resp = Response::REPLAY;
+          } else if (xc >= 0 && xc < 8 && yc >= 0 && yc < 8) {
+            resp = Response::CELL;
+          } else {
+            xc = -1;
+            yc = -1;
+          }
+          break;
+        case SDL_WINDOWEVENT:
+          if (windowEvent.window.event == SDL_WINDOWEVENT_SIZE_CHANGED) {
+            board->reset_window_size();
+            break;
+          }
         }
-        return {resp, xc, yc};
+        if (resp != Response::OK)
+          break;
+      }
     }
+    return {resp, xc, yc};
+  }
 
-    Response wait() const
-    {
-        SDL_Event windowEvent;
-        Response resp = Response::OK;
-        while (true)
-        {
-            if (SDL_PollEvent(&windowEvent))
-            {
-                switch (windowEvent.type)
-                {
-                case SDL_QUIT:
-                    resp = Response::QUIT;
-                    break;
-                case SDL_WINDOWEVENT_SIZE_CHANGED:
-                    board->reset_window_size();
-                    break;
-                case SDL_MOUSEBUTTONDOWN: {
-                    int x = windowEvent.motion.x;
-                    int y = windowEvent.motion.y;
-                    int xc = int(y / (board->H / 10) - 1);
-                    int yc = int(x / (board->W / 10) - 1);
-                    if (xc == -1 && yc == 8)
-                        resp = Response::REPLAY;
-                }
-                break;
-                }
-                if (resp != Response::OK)
-                    break;
-            }
+  Response wait() const {
+    SDL_Event windowEvent;
+    Response resp = Response::OK;
+    while (true) {
+      if (SDL_PollEvent(&windowEvent)) {
+        switch (windowEvent.type) {
+        case SDL_QUIT:
+          resp = Response::QUIT;
+          break;
+        case SDL_WINDOWEVENT_SIZE_CHANGED:
+          board->reset_window_size();
+          break;
+        case SDL_MOUSEBUTTONDOWN: {
+          int x = windowEvent.motion.x;
+          int y = windowEvent.motion.y;
+          int xc = int(y / (board->H / 10) - 1);
+          int yc = int(x / (board->W / 10) - 1);
+          if (xc == -1 && yc == 8)
+            resp = Response::REPLAY;
+        } break;
         }
-        return resp;
+        if (resp != Response::OK)
+          break;
+      }
     }
+    return resp;
+  }
 
-  private:
-    Board *board;
+private:
+  Board *board;
 };
